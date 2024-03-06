64964c35359f1a12c5f7dcaaa7d0e64427b05d0e
vasilii_tarasov <tarasov_vas@mail.ru>
2024-03-06 13:18:33 +0300

    first

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Profile from './pages/profile.tsx'
+import Home from './pages/home.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Profile />
+    <Home />
   </React.StrictMode>,
 )
